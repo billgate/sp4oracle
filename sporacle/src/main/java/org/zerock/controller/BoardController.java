b3af4d686eedd68c3936dd4744cb332d86f09a10
zerock <zerockcode@gmail.com>
2016-05-31 22:55:10 +0900

    ADD Method/ Domain Class

--- sporacle/src/main/java/org/zerock/controller/BoardController.java
+++ sporacle/src/main/java/org/zerock/controller/BoardController.java
@@ -1,10 +1,22 @@
 package org.zerock.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.domain.BoardVO;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+	
+    private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 
+  @RequestMapping(value = "/register", method = RequestMethod.GET)
+  public void registerGET(BoardVO vo, Model model) throws Exception {
+      logger.info("register get..........");
+  }
+	
 }
